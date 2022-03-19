f52f80f01a27115b6df57d55bf75c33961b38744
권기남 <ginam@gwonginam-ui-MacBookAir.local>
2022-03-19 16:04:15 +0900

    Add Product model

---
+++ models/productModel.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const productSchema = mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        price: {
+            type: Number,
+            required: true,
+        },
+        brand: {
+            type: String,
+            required: true,
+        },
+        category: {
+            type: String,
+            required: true,
+        },
+        description: {
+            type: String,
+            required: true,
+        }
+    },
+    {
+        timestamps: true
+    }
+)
+
+const Product = mongoose.model('product', productSchema);
+
+export default Product;
