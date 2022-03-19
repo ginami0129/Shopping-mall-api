83b557076d56b7512acd979995f71d6ee77b7839
권기남 <ginam@gwonginam-ui-MacBookAir.local>
2022-03-19 16:44:14 +0900

    Add Delete func

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,31 +1,59 @@
 import express from "express"
+import asyncHandler from "express-async-handler";
+import Product from "../models/productModel.js";
 
 const router = express.Router()
 
 
 
 // CRUD
-router.get('/', (req, res) => {
+
+// product 전체 불러오는 API
+router.get('/', asyncHandler( async(req, res) => {
+    const products = await Product.find()
     res.json({
-        msg: 'Product Get!'
+        count: products.length,
+        products: products
     })
-})
+}))
 
-router.post('/',(req, res) => {
+// 상세 프로덕트 불러오는 api
+router.get('/:productID', asyncHandler(async (req, res) => {
+    const id = req.params.productID;
+    const product = await Product.findById(id);
+    res.json(product);
+}))
 
-    const userInput = {
+router.post('/', asyncHandler(async (req, res) => {
+
+    // const userInput = {
+    //     name: req.body.name,
+    //     price: req.body.price,
+    //     brand: req.body.brand,
+    //     description: req.body.desc
+    // }
+    //
+    // res.json({
+    //     msg: 'Created Product',
+    //     product: userInput
+    // })
+
+    const newProduct = new Product({
         name: req.body.name,
         price: req.body.price,
         brand: req.body.brand,
-        description: req.body.desc
-    }
+        category: req.body.category,
+        description: req.body.desc,
+    });
+
+    const createdProduct = await newProduct.save();
 
     res.json({
-        msg: 'Created Product',
-        product: userInput
+        msg: "Created Product",
+        product: createdProduct,
     })
 
-})
+}))
 
 router.put('/', (req, res) => {
     res.json( {
@@ -33,11 +61,23 @@ router.put('/', (req, res) => {
     })
 })
 
-router.delete('/', (req, res) => {
+router.delete('/', asyncHandler( async(req, res) => {
+    await Product.remove();
     res.json({
-        msg: 'Deleted Product'
+        msg: 'Deleted all Product'
     })
-})
+}))
+
+// 한개의 제품 삭제
+router.delete('/:productID', asyncHandler( async (req, res) => {
+
+    const id = req.params.productID;
+    // remove delete
+    await Product.findByIdAndRemove(id);
+    res.json({
+        msg: "deleted at "+ id,
+    })
+}))
 
 
 
