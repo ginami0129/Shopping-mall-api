2e4f938af5f58a1f835bbc04669d8f490aab724a
Ginam Kwon <ginami0129g@gmail.com>
2022-04-03 11:23:32 +0900

    Add pagenation and keyword

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -12,9 +12,26 @@ const router = express.Router();
 router.get(
   "/",
   asyncHandler(async (req, res) => {
-    const products = await Product.find();
+    const pageSize = 2;
+    const page = Number(req.query.pageNumber) || 1;
+
+    const keyword = req.query.keyword
+      ? {
+          name: {
+            $regex: req.query.keyword,
+            $options: "i",
+          },
+        }
+      : {};
+
+    const count = await Product.countDocuments({ ...keyword });
+    const products = await Product.find({ ...keyword })
+      .limit(pageSize)
+      .skip(pageSize * (page - 1));
     res.json({
       count: products.length,
+      page,
+      pages: Math.ceil(count / pageSize),
       products: products,
     });
   })
